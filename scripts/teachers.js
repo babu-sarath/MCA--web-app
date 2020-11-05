13281001c025588ec189cebd73d01bc78fd5b03a
sarathchandra babu <sarathbabu4321@gmail.com>
2020-11-05 21:16:26 +0530

    first commit

---
+++ scripts/teachers.js
@@ -0,0 +1,40 @@
+let db = firebase.firestore()
+let auth = firebase.auth()
+
+$('#createTeacher').submit((e) => {
+	e.preventDefault()
+	let id = $('#teacherID').val()
+	let nameVal = $('#name').val()
+	let dobVal = $('#dob').val()
+	let emailVal = $('#email').val()
+	let passwordVal = $('#password').val()
+	let phoneNoVal = $('#phoneNo').val()
+	let userTypeVal = 'teacher'
+
+	// Add a new document in collection "users"
+	db.collection('users')
+		.doc(id)
+		.set({
+			name: nameVal,
+			dob: dobVal,
+			email: emailVal,
+			phone: phoneNoVal,
+			userType: userTypeVal,
+		})
+		.then(() => {
+			console.log('Document successfully written!')
+
+			auth.createUserWithEmailAndPassword(emailVal, passwordVal)
+				.then((user) => {
+					console.log('Created User', user)
+				})
+				.catch((error) => {
+					console.log('Error ', error)
+				})
+
+			$('#createTeacher').trigger('reset')
+		})
+		.catch((error) => {
+			console.error('Error writing document: ', error)
+		})
+})
